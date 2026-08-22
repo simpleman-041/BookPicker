eb7c321d20adaad0db1affc9eeb812a1acb9efef
simpleMan-041 <dai2525yuu@icloud.com>
2026-08-22 14:00:43 +0900

    Controler骨格作成

---
+++ BookPicker/Controllers/BooksController.cs
@@ -0,0 +1,19 @@
+﻿using BookPicker.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BookPicker.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class BooksController : ControllerBase
+    {
+        // コンストラクタで受け取った BookPickerDbContext をプライベートフィールドに格納しておく
+        private readonly BookPickerDbContext _dbContext;
+
+        public BooksController(BookPickerDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+    }
+}
